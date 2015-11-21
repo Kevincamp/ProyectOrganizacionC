aeb21826625bf226be6ee75af885d4033580eb4e
KevinCamp <kevincamp_90@me.com>
2015-11-21 11:43:11 -0500

    cambios para el menu
    Cambios para el menú y la recepción de String.

--- main.c
+++ main.c
@@ -7,42 +7,44 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 void sumaHexadecimal(char a, char b);
+void sumaDecimal();
 
 int main(int argc, const char * argv[]) {
-    int x;
-    printf("Proyecto de 1er Parcial de Organización y Arquitectura de Computadores!\n");
-    printf("Integrantes: \n");
-    printf("\t - Kevin Campuzano.\n");
-    printf("\t - Oswaldo Bayona.\n");
-    printf("\t - Jorge Enrique.\n");
-    printf("*************************************************************************************\n");
-    
-    printf("\t\t\t\t Bienvenido.\n");
-    printf("\t 1. Sumar números decimales.\n");
-    printf("\t 2. Sumar números hexadecimales.\n");
-    printf("\t 3. Sumar numeros decimales y hexadecimales\n");
-    
-    switch (x) {
-        case 1:
-            printf("Escogio opcion 1");
-            break;
-        case 2:
-            printf("Escogio opcion 2");
-            break;
-        case 3:
-            printf("Escogio opcion 3");
-            break;
-        default:
-            printf("Error ingrese la opcion correcta.");
-            break;
-    }
-    
-    printf("Por favor escoja su opcion:\n");
-    scanf("%i",&x);
-    
-    return 0;
-    
+    int seleccion;
+    do{
+        printf("Proyecto de 1er Parcial de Organización y Arquitectura de Computadores!\nIntegrantes: \n");
+        printf("\t - Kevin Campuzano.\n");
+        printf("\t - Oswaldo Bayona.\n");
+        printf("\t - Jorge Enrique.\n");
+        do{
+            printf("\t\t\t\t Bienvenido.\n");
+            printf("\t 1. Sumar números decimales.\n");
+            printf("\t 2. Sumar números hexadecimales.\n");
+            printf("\t 3. Sumar numeros decimales y hexadecimales\n");
+            printf("\t 4. Salir.\n");
+            printf("Escriba su opción:\n");
+            scanf("%i",&seleccion);
+        }while (seleccion !=1 && seleccion !=2 && seleccion !=3 && seleccion !=4);
+        
+        if(seleccion == 1){ /* Sumatoria Decimales*/
+            sumaDecimal();
+        }
+    }while (seleccion !=4);
+}
+
+
+void sumaDecimal (){
+    double sum;
+    char a[256], b[256];
+    printf("\t\t\t*** Suma de Decimales ***\n");
+    printf("\n Ingrese 1er Número: ");
+    fgets (a, 100, stdin);
+    printf("\n Ingrese 2do Número: ");
+    fgets (b, 100, stdin);
+    sum = atof(a) + atof(b);
+    printf("\n %s + %s = %f \n", a,b,sum);
 }
 
 void sumaHexadecimal(char a, char b){
